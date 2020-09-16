8e4e1b76162ce5dfcc03afcf9f97534c1845edd9
wilcesde <wilcascesde@gmail.com>
2020-09-15 22:03:55 -0500

    first commit

---
+++ ejemploDos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ejemploDos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CONCESIONARIO");
+
+            Console.WriteLine("Marca");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Serie");
+            string serie = Console.ReadLine();
+            Console.WriteLine("Modelo");
+            short modelo = short.Parse(Console.ReadLine());
+            Console.WriteLine("Placa");
+            string placa = Console.ReadLine();
+
+            //Instanciar la class vehiculo
+            var vehiculoUno = new Vehiculo()
+            {
+                Marca = marca,
+                Serie = serie,
+                Modelo = modelo,
+                Placa = placa
+            };
+
+            Console.WriteLine(vehiculoUno.Placa);
+        }
+    }
+}
---
+++ ejemploDos/Vehiculo.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace ejemploDos
+{
+    class Vehiculo
+    {
+        //Propiedades
+        public string Marca { get; set; }
+        public string Serie { get; set; }
+
+        short modelo;
+        public short Modelo { 
+            get
+            {
+                return modelo;
+            }
+            set
+            {
+                if (value > 2017 && value < 2022)
+                {
+                    modelo = value;
+                }
+                else
+                {
+                    Console.WriteLine("Modelo no valido");
+                }                
+            }
+        }
+
+
+        string placa;
+        public string Placa {
+            get
+            {
+                return placa;
+            }
+            set
+            {
+                if (value.Length == 6)
+                {
+                    placa = value;
+                }
+                else
+                {
+                    Console.WriteLine("La placa debe ser de 6 digitos");
+                }
+            }
+        }
+
+
+        //Metodos
+        public void prender()
+        {
+            Console.WriteLine("El vehiculo esta prendiendo");        }
+    }
+}
